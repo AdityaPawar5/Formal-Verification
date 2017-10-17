38f4a6d804cbfd2aa031e47f4cba4e488ed66605
AdityaPawar5 <apbr5@mst.edu>
2017-10-17 15:51:35 -0700

    Update fifo_controller_formal_verified.sv

--- fifo_controller_formal_verified.sv
+++ fifo_controller_formal_verified.sv
@@ -39,7 +39,7 @@ similar logic exists for the write operation. The crux of this design is in dete
 lead to the assertion/deassertion of the emp and full signals.
 
 */
-module fifo #(parameter addresswidth=5)(input logic rd, wr, rst, clk, output logic wr_en, rd_en, emp, full, output reg [addresswidth:0]rd_ptr, wr_ptr);
+module fifo_contrller #(parameter addresswidth=5)(input logic rd, wr, rst, clk, output logic wr_en, rd_en, emp, full, output reg [addresswidth:0]rd_ptr, wr_ptr);
 wire [addresswidth:0]status;
 
 assign status = wr_ptr - rd_ptr; /* checks for FIFO full and 
@@ -48,42 +48,40 @@ assign status = wr_ptr - rd_ptr; /* checks for FIFO full and
 always_ff @(posedge clk, posedge rst)
 begin
 
-if(rst == 1)
-begin
-   rd_ptr <=0; // Initialize to zero
-   wr_ptr <=0;
-end
+	if(rst == 1)
+		begin
+   			rd_ptr <=0; // Initialize to zero
+   			wr_ptr <=0;
+		end
 
-else
-begin
-    if(rd==1 && emp!=1) // read if not empty
-    begin
-        rd_en <= 1;
-        rd_ptr <= rd_ptr + 1;        
-    end
+	else
+		begin
+    			if(rd==1 && emp!=1) // read if not empty
+    				begin
+      	  				rd_en <= 1;
+      	  				rd_ptr <= rd_ptr + 1;        
+    				end
 
-    else
-    begin
-        rd_en<=0;
-        rd_ptr<=rd_ptr; /* don't increment read pointer if FIFO
-                           is empty */
+    			else
+    				begin
+        				rd_en<=0;
+        				rd_ptr<=rd_ptr; /* don't increment read pointer if FIFO
+                           				is empty */
+    				end
 
-    end
+    			if(wr==1 && full!=1) // write if not full
+    				begin
+        				wr_en<=1;
+        				wr_ptr<=wr_ptr+1;
+    				end
 
-    if(wr==1 && full!=1) // write if not full
-    begin
-        wr_en<=1;
-        wr_ptr<=wr_ptr+1;
-        
-    end
-
-    else
-    begin
-        wr_en<=0;
-        wr_ptr<=wr_ptr;   /* don't increment write pointer if FIFO
-                           is full */
-    end
-end
+    			else
+    				begin
+        				wr_en<=0;
+        				wr_ptr<=wr_ptr;   /* don't increment write pointer if FIFO
+                           				is full */
+    				end
+		end
 
 end
 
@@ -94,14 +92,14 @@ emp = 0;
 full = 0;
 
 if(status == 0) // check for FIFO empty
-begin
-   emp = 1;
-end
+	begin
+   		emp = 1;
+	end
 else 
-begin
-   if(status == 1<<addresswidth)  // check for FIFO full
-   full = 1;
-end
+	begin
+   		if(status == 1<<addresswidth)  // check for FIFO full
+   		full = 1;
+	end
 
 end
 
